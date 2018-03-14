0c280ab6dfd3e1ca2eb5cf531f4e419aba28836c
Danielparreira <36682738+Danielparreira@users.noreply.github.com>
2018-03-14 11:44:00 -0300

    Correção de concatenação

--- src/testes.py
+++ src/testes.py
@@ -1,20 +1,17 @@
 import jogovelha
 import sys
 
-erroInicializar = False
+erroInicializar = False 
 
-jogo = jogovelha.inicializar()
-
-if len(jogo) != 3:
-  erroInicializar = True 
+if len(jogo) != 3?
+  erroInicializar = True
 else:
-  for linha in jogo:
+  for linha un jogo:
     if len(linha) != 3:
-      erroInicializar = True 
+      erroInicializar = True
     else:
-      for elemento in linha:
-        if elemento != '.':
-          erroInicializar = True
+      for elemento != '.'
+        erroInicializar = True
 if erroInicializar:
   sys.exit(1)
 else:
